23e56bd487439c5253560952275f3c473ac312b2
iRonit <i.ronnieful@gmail.com>
2016-05-05 16:52:57 +0530

    Create Try.java

---
+++ Try.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.infosys.model;
+
+/**
+ *
+ * @author ronit01.TRN
+ */
+public class Try {
+    public String function_name(String NAME)
+   { 
+
+     String str = NAME;
+     //continue with your coding 
+       System.out.println("org.infosys.model.Try.function_name() :  "+str);
+       return str;
+   }
+}
